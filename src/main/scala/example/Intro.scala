13efa274c20ce92d862e6e2b168a5a62839b190a
Paolo Picci <paolo.picci@sky.uk>
2019-01-29 14:15:53 +0000

    adding direct and cp style

--- src/main/scala/example/Intro.scala
+++ src/main/scala/example/Intro.scala
@@ -23,5 +23,31 @@ object Intro {
 
 object DirectStyle {
 
+  // c * c == a * a + b * b
+
+  def square(x: Int): Int = x * x
+
+  def sum(x: Int, y: Int): Int = x + y
+
+  def pitagoras(a: Int, b: Int): Int = {
+    val aa = square(a)
+    val bb = square(b)
+    sum(aa, bb)
+  }
+
+}
+
+object ContinuationPassingStyle {
+
+  def square(x: Int, k: Int => Int): Int = k(x * x)
+
+  def sum(x: Int, y: Int, k: Int => Int): Int = k(x + y)
+
+  def pitagoras(a: Int, b: Int, k: Int => Int): Int =
+    square(a, aa =>
+      square(b, bb =>
+        sum(aa, bb, k)
+      )
+    )
 
 }
