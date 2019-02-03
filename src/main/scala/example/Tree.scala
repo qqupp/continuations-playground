553ca4e7abd8642eca488203bd989b805c279370
Paolo <paolo.picci@gmail.com>
2019-02-03 11:02:10 +0000

    Introducint data package

--- src/main/scala/example/Tree.scala
+++ src/main/scala/example/Tree.scala
@@ -1,9 +1,7 @@
 package example
 
+import Tree._
 
-sealed trait Tree[T] extends Product with Serializable
-final case class Node[T](left: Tree[T], right: Tree[T]) extends Tree[T]
-final case class Leaf[T](value: T) extends Tree[T]
 
 object TreeCont {
 
@@ -30,18 +28,5 @@ object TreeNaive {
     case Node(l, r) => Node(map(l)(f), map(r)(f))
   }
 
-}
-
-object TreeUtils {
-
-  def deepTree: Tree[Int] = {
-    def loop(n: Int, acc: Tree[Int]): Tree[Int] =
-      if (n <= 0)
-        acc
-      else
-        loop(n - 1, Node(acc, Leaf(0)))
-
-    loop(50000, Leaf(1))
-  }
 }
 
