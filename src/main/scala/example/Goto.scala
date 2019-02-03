75af78f6c53a2cd0c0863a19611aa2648d7144ef
Paolo <paolo.picci@gmail.com>
2019-02-03 12:00:44 +0000

    Adding goto continuation example

---
+++ src/main/scala/example/Goto.scala
@@ -0,0 +1,53 @@
+package example
+
+import data.continuation.basic.Continuation
+
+object Goto extends App {
+
+  val addressTable = new scala.collection.mutable.HashMap[String, Unit => Unit]()
+
+  def LABEL(value: String) = Continuation[Unit, Unit]{ k => addressTable(value) = k; k()}
+  def GOTO(value: String)= Continuation[Unit, Unit](k => addressTable(value)())
+  def CONTINUE = Continuation[Unit, Unit](k => k())
+
+  var counter = 0
+  var game = 0
+
+  val program = for {
+    _ <- LABEL("START")
+    _ =  counter = 1
+    _ =  game += 1
+    _ =  println(s"-- Round $game --")
+    _ <- LABEL("INCREMENT")
+    _ =  println(s" counter = $counter")
+    _ =  counter += 1
+    _ <- if (game > 2 && counter > 3)
+           CONTINUE
+         else if (counter <= 3)
+           GOTO("INCREMENT")
+         else
+           GOTO("START")
+    _ = println("-- Bye Bye --")
+  } yield ()
+
+  program.run(x => x)
+
+  /*
+  output
+
+    -- Round 1 --
+     counter = 1
+     counter = 2
+     counter = 3
+    -- Round 2 --
+     counter = 1
+     counter = 2
+     counter = 3
+    -- Round 3 --
+     counter = 1
+     counter = 2
+     counter = 3
+    -- Bye Bye --
+ */
+
+}
